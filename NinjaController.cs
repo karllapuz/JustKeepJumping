48082c2a594f439f87e6add17a539c5da610dcf4
Karl Lapuz <karllapuz25@gmail.com>
2018-10-13 23:25:52 -0700

    add flip logic

---
+++ NinjaController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NinjaController : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float move = Input.GetAxis("Horizontal");
+        rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+        if (move > 0 && !facingRight) {
+            Flip();
+        }
+        else if (move < 0 && facingRight) {
+            Flip();
+        }
+
+	}
+
+    void Flip() {
+
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
